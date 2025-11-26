8a864a6ab164b1fcc5d7aab3e1bb1fff8489fb46
vega <vegalin1029@gmail.com>
2025-11-26 18:47:24 +0800

    Import initTables in server index file

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import "./initTables"; // ← 新增這個
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
